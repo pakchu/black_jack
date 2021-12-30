d932893f26329370ceda24a30d2888bfa7652cfa
pakchu <gus4734@gmail.com>
2021-12-31 00:16:30 +0900

    trump_deck set up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
 use std::io;
+use rand::prelude::*;
+
+struct Trumps<'a>{
+    number: u32,
+    shapes: &'a str,
+}
+
+fn init_trump_deck() -> Vec<Trumps<'static>> {
+    let mut deck: Vec<Trumps> = Vec::new();
+    for num in 1..13{
+        for shape in vec!["spade", "clover", "heart", "diamond"] {
+            deck.push(Trumps{
+                number : num,
+                shapes : shape,
+            });
+        }
+    }
+    let mut rng = rand::thread_rng();
+
+    deck.shuffle(&mut rng);
+
+    return deck;
+}
+
 fn main() {
     // intro
     println!("Welcom to Black Jack game!");
@@ -15,6 +39,9 @@ fn main() {
     let num_of_player: i32 = num_of_player_str.parse().unwrap();
 
     println!("you chose {}", num_of_player);
+    
+    // init trump deck (shuffled)
+    init_trump_deck();
 
     
 }
